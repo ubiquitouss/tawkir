8d39ba67350b6da22da959eba91438188951045b
root <root@TAWKIR.localdomain>
2021-10-12 22:12:01 +0600

    list page is working perfectly

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -15,10 +20,15 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10,
-    choices=STATUS_CHOICES,
-    default='draft')
+    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
     class Meta:
         ordering = ('-publish',)
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+        args=[self.publish.year,
+        self.publish.month,
+        self.publish.day, self.slug])
